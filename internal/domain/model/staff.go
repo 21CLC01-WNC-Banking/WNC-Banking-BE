0c2fdd9dc87b71ab228dd05b774725809457fad8
Tricker14 <trietthetricker@gmail.com>
2024-12-27 23:05:25 +0700

    [WNC-32] add message when staff add amount for account

--- internal/domain/model/staff.go
+++ internal/domain/model/staff.go
@@ -5,6 +5,7 @@ import "time"
 type AddAmountToAccountRequest struct {
 	Amount        int64  `json:"amount" binding:"required,gt=0"`
 	AccountNumber string `json:"accountNumber" binding:"required"`
+	Message       string `json:"message"`
 }
 
 type GetTransactionsResponse struct {
