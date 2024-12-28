75a9df914bf4d037249e78c2bc7195650cc4eef5
hoadang0305 <dangnhathoa22@gmail.com>
2024-12-28 12:43:38 +0700

    [WNC-24] fix migration add staff

--- migrations/20241227150117_insert_staff.up.sql
+++ migrations/20241227150117_insert_staff.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (`email`, `name`, `role_id`, `phone_number`, `password`)
 VALUES
-('admin@internetbanking.com', 'Staff', '2', '0123987456', '$2a$10$ka/P7URqQrKweoQo.9491.yNl3sBA.vm7LHlhJRtRugkM/U8jR5Dy');
+('admin@internetbanking.com', 'Staff',  (SELECT id FROM roles WHERE name = 'staff'), '0123987456', '$2a$10$ka/P7URqQrKweoQo.9491.yNl3sBA.vm7LHlhJRtRugkM/U8jR5Dy');
