f2a848259cb17ba997eb40cca2002672becdd5ad
hoadang0305 <dangnhathoa22@gmail.com>
2025-01-02 09:48:54 +0700

    [WNC-46] middleware for RSA

---
+++ migrations/20250101161639_insert_partner_bank.up.sql
@@ -0,0 +1,18 @@
+INSERT INTO partner_banks (`bank_code`, `bank_name`, `short_name`, `logo_url`, `research_api`, `transfer_api`, `public_key`)
+VALUES
+    ('BANK1', 'demo 1', 'abc', 'https://vi.m.wikipedia.org/wiki/T%E1%BA%ADp_tin:Nam_A_Bank_Logo.jpg', 'api', 'api', '-----BEGIN PUBLIC KEY-----
+ MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHu1VG9g3uSeKB5IsPt+XOjlgl3l
+ F8sVtgiemOsHJEjF6BqQwVoUiebdR0BW9c+Mq4IB+RkFjYGHO3FzRlLzg2zYQ6X8
+ KpGFRHUfOdljXJn05BFP0qdVrOVW8KNxwEf/l96lQ/XUd2zjZWf/NUwCL+N3trAQ
+ NwUD/EqR8uZ735J/AgMBAAE=
+ -----END PUBLIC KEY-----');
+INSERT INTO partner_banks (`bank_code`, `bank_name`, `short_name`, `logo_url`, `research_api`, `transfer_api`, `public_key`)
+VALUES
+    ('BANK2', 'demo 2', 'abcd', 'https://vi.m.wikipedia.org/wiki/T%E1%BA%ADp_tin:Nam_A_Bank_Logo.jpg', 'api', 'api', '-----BEGIN PUBLIC KEY-----
+ MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHu1VG9g3uSeKB5IsPt+XOjlgl3l
+ F8sVtgiemOsHJEjF6BqQwVoUiebdR0BW9c+Mq4IB+RkFjYGHO3FzRlLzg2zYQ6X8
+ KpGFRHUfOdljXJn05BFP0qdVrOVW8KNxwEf/l96lQ/XUd2zjZWf/NUwCL+N3trAQ
+ NwUD/EqR8uZ735J/AgMBAAE=
+ -----END PUBLIC KEY-----');
+
+
