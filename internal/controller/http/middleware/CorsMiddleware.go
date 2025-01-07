d21ee24a9a9d0e4be494f6e50d50af3120ba4e1e
Tricker14 <trietthetricker@gmail.com>
2025-01-07 11:03:10 +0700

    [WNC-64] refractor cors functions

--- internal/controller/http/middleware/CorsMiddleware.go
+++ internal/controller/http/middleware/CorsMiddleware.go
@@ -1,15 +1,20 @@
 package middleware
 
 import (
+	"github.com/21CLC01-WNC-Banking/WNC-Banking-BE/internal/utils/env"
 	"github.com/gin-gonic/gin"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
+	url, err := env.GetEnv("URL")
+	if err != nil {
+		panic(err)
+	}
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", url)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
 			return
