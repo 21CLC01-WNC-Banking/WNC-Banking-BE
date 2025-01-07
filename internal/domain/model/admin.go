a233c57b2ff2c646146aa00a25fa7b56ef0f982b
hoadang0305 <dangnhathoa22@gmail.com>
2025-01-07 21:53:21 +0700

    [WNC-66] add limit pw

--- internal/domain/model/admin.go
+++ internal/domain/model/admin.go
@@ -6,7 +6,7 @@ type CreateStaffRequest struct {
 	Email       string `json:"email" binding:"required,email"`
 	Name        string `json:"name" binding:"required"`
 	PhoneNumber string `json:"phoneNumber" binding:"required"`
-	Password    string `json:"password" binding:"required"`
+	Password    string `json:"password" binding:"required, min=8,max=255"`
 }
 
 type CreateStaffResponse struct {
